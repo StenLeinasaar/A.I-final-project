7c96eb6b896225ecae84fec782f0023f3952960a
Sten Leinasaar <stenleinasaar@gmail.com>
2023-03-22 09:49:52 -0400

    Fixing a bug

--- ai_players/alpha_beta_pruning.py
+++ ai_players/alpha_beta_pruning.py
@@ -4,7 +4,7 @@ import sys
 def max_val(game_board:Board, depth: int, alpha: int, beta: int, player:int) -> int:
     # print(f"max val called with depth {depth}")
     if depth == 0: #or game_board.get_state() in [0,-1,1]: # or (node is either draw, lose or win.
-        return evaluate(game_board, player)
+        return evaluate(game_board, player,True)
     best_value = -float("inf")
     moves = game_board.get_possible_moves() #NEED a function to get all possible moves
     for move in moves:
@@ -24,7 +24,7 @@ def min_val(game_board:Board, depth: int, alpha: int, beta: int, player:int) -> 
     # print(f"Min val function called, depth is {depth}")
     if depth == 0: #or game_board.get_state() in [0,-1,1]: # or (node is either draw, lose or win.
         # print("returning evaluate score to main function")
-        return evaluate(game_board, player)
+        return evaluate(game_board, player, False)
     best_value = float("inf")
     moves = game_board.get_possible_moves()
     for move in moves:
@@ -55,26 +55,32 @@ def alpha_beta_pruning(game_board:Board, player:int):
             game_board.play(player,move)
             value = max_val(game_board, depth, alpha, beta, player)
             if value > max_value:
+                max_value = value
                 best_move = move
             game_board.undo(move)
             
     else:
         # min player
         for move in available_moves:
+            print("min player")
             # print(f"checking min player with the move {move}")
             game_board.play(player,move)
             # print("made a move, printing a game board")
             # print(f'calling a min player, depth is {depth}')
             value = min_val(game_board, depth, alpha, beta, player)
             if value < min_value:
+                min_value = value
                 best_move = move
             game_board.undo(move)
+
+    print(f"the min value is {min_value}")
+    print(f"the move chosen is {best_move}")
     
     return best_move
 
 
 # node state can be set here
-def evaluate(game_board: Board, player:int):
+def evaluate(game_board: Board, player:int, max_player):
     """
     Evaluate the current board state and return a score for the given player.
     """
@@ -82,12 +88,19 @@ def evaluate(game_board: Board, player:int):
     # This might not be a correct way. Make sure you can refer to an opponent as well
     opponent = 3 - player
     score = 0
+    max_player = max_player
 
     # Check for winning positions
     if game_board.is_win(player):
-        score += 1000
+        if max_player:
+            score += 1000
+        else:
+            score -= 1000
     elif game_board.is_win(opponent):
-        score -= 1000
+        if max_player:
+            score -= 1000
+        else:
+            score += 1000
 
     # Count open rows, columns, and diagonals
     open_fours = 0
@@ -105,22 +118,36 @@ def evaluate(game_board: Board, player:int):
                     elif count == 2:
                         open_twos += 1
 
-    # Add score based on open rows, columns, and diagonals
-    score += 100 * open_fours + 10 * open_threes + open_twos
+    if max_player:
+        # Add score based on open rows, columns, and diagonals
+        score += 100 * open_fours + 10 * open_threes + open_twos
+    else:
+        score -= 100 * open_fours + 10 * open_threes + open_twos
 
     # Subtract penalty for opponent's open rows, columns, and diagonals
+    open_fours = 0
+    open_threes = 0
+    open_twos = 0
     for i in range(game_board.size):
         for j in range(game_board.size):
             if game_board.grid[i][j] == opponent:
                 if game_board.has_neighbor(i, j):
                     count = game_board.count_open(i, j)
                     if count >= 4:
-                        score -= 100
+                        open_fours += 1
                     elif count == 3:
-                        score -= 10
+                        open_threes += 1
                     elif count == 2:
-                        score -= 1
+                        open_twos += 1
+    print(f"Score currently is {score}")
     # print(f"Returning the score of  {score}")
+    if max_player:
+        # Sbutract based on open rows, columns, and diagonals
+        score -= 1000 * open_fours + 100 * open_threes + 10*open_twos
+    else:
+        score += 1000 * open_fours + 100 * open_threes + 10*open_twos
+    
+    print(f"Score after punishing is {score}")
 
     return score
 
